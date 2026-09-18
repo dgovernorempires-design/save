cf993938e13838e5de045a576813f8a4ecc18490
dgovernorempires-design <dgovernorempires@gmail.com>
2026-09-18 18:26:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const { execSync, spawn } = require('child_process');
 const path = require('path');
 
 const app = express();
+// Enable CORS for all incoming requests from your cPanel frontend
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Top-Text, X-Bottom-Text');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
